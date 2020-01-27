4ac1e60eccdf6e59fedc57e9b52e8b58674a1cdf
Dhanalakshmi Narala <dhanalakshmi.narala@everest.engineering>
2020-01-27 11:30:54 +0530

    Refactor the code

--- FizzBuzz/test/fizzBuzzTest.js
+++ FizzBuzz/test/fizzBuzzTest.js
@@ -22,7 +22,7 @@ describe('fizzBuzz', () => {
         let expectedResult = [1, 2, 'Fizz', 4, 'Buzz', 'Fizz', 7, 8, 'Fizz', 'Buzz', 
                             11, 'Fizz', 13, 14, 'Fizz Buzz', 16, 17, 'Fizz', 19, 'Buzz',
                             'Fizz', 22, 23, 'Fizz', 'Buzz', 26, 'Fizz', 28, 29, 'Fizz Buzz', 
-                            31, 32, 'Fizz', 34, 'Buzz', 'Fizz']
-        fizzBuzz(1, 36).should.deep.equal(expectedResult)
+                            31, 32, 'Fizz', 34, 'Buzz', 'Fizz'];
+        fizzBuzz(1, 36).should.deep.equal(expectedResult);
     })
 })
