34b8dc754b152f6ce923dc4bfdc50abbd1515109
Dhanalakshmi Narala <dhanalakshmi.narala@everest.engineering>
2020-01-27 10:32:11 +0530

    Add unit test for checking large range of number

--- FizzBuzz/test/fizzBuzzTest.js
+++ FizzBuzz/test/fizzBuzzTest.js
@@ -17,4 +17,12 @@ describe('fizzBuzz', () => {
     it('should return same number for neither multiple of 3 nor multiple of 5', () => {
         fizzBuzz(11, 11).should.deep.equal([11]);
     })
+
+    it('same pattern should work for large range of numbers', () => {
+        let expectedResult = [1, 2, 'Fizz', 4, 'Buzz', 'Fizz', 7, 8, 'Fizz', 'Buzz', 
+                            11, 'Fizz', 13, 14, 'Fizz Buzz', 16, 17, 'Fizz', 19, 'Buzz',
+                            'Fizz', 22, 23, 'Fizz', 'Buzz', 26, 'Fizz', 28, 29, 'Fizz Buzz', 
+                            31, 32, 'Fizz', 34, 'Buzz', 'Fizz']
+        fizzBuzz(1, 36).should.deep.equal(expectedResult)
+    })
 })
