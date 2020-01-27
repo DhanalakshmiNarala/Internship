590189352d02af14d41d36bbd833ba2b9cc3f212
Dhanalakshmi Narala <dhanalakshmi.narala@everest.engineering>
2020-01-27 11:26:31 +0530

    Add unit test for checking very big non prime numbers

--- PrimeNumber/test/isPrimeNumberTest.js
+++ PrimeNumber/test/isPrimeNumberTest.js
@@ -9,4 +9,8 @@ describe('Prime numbers', () => {
     it('should return true for prime number 9999999967', () => {
         isPrimeNumber(9999999967).should.be.true;
     })
+
+    it('should return false for non prime number 1000000000', () => {
+        isPrimeNumber(1000000000).should.be.false;
+    })
 })
