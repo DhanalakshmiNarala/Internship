a6f2474dc12080c131324a5e86f06a3c54bafdd5
Dhanalakshmi Narala <dhanalakshmi.narala@everest.engineering>
2020-01-27 10:09:26 +0530

    Add unit test for checking Fizz Buzz case

--- FizzBuzz/test/fizzBuzzTest.js
+++ FizzBuzz/test/fizzBuzzTest.js
@@ -5,7 +5,12 @@ describe('fizzBuzz', ()=> {
     it('should return fizz for multiple of 3', ()=> {
         fizzBuzz(6, 6).should.deep.equal(["fizz"]);
     })
+
     it('should return buzz for multiple numbers of 5', ()=> {
         fizzBuzz(10, 10).should.deep.equal(["buzz"])
+    })
+
+    it("should return fizzbuzz for multiple number of 3 and 5", ()=> {
+        fizzBuzz(15, 15).should.deep.equal(["fizzbuzz"])
     })
 })
