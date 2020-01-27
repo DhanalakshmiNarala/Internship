ed411dc3935337b2403285462ab20b7cbe3e3e2a
Dhanalakshmi Narala <dhanalakshmi.narala@everest.engineering>
2020-01-27 10:59:16 +0530

    Add unit test for checking large range of numbers

--- Fibonnaci/test/fibonnaciTest.js
+++ Fibonnaci/test/fibonnaciTest.js
@@ -13,4 +13,11 @@ describe('Fibonnaci numbers', () => {
     it('should return 8 form 6th fibonnaci number', () => {
         fibonnaciNumbers(8, 8).should.deep.equal([21])
     })
+
+    it('should work for large range numbers', () => {
+        let expectedResult = [1, 1, 2, 3, 5, 8, 13, 21, 34, 55]
+        fibonnaciNumbers(1, 10).should.deep.equal(expectedResult)
+    })
+
+
 })
