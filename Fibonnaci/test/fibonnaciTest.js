71b51fa6ba32896bef91c191f90d39903512e1f3
Dhanalakshmi Narala <dhanalakshmi.narala@everest.engineering>
2020-01-27 11:04:22 +0530

    Add unit test for handling very big input values

--- Fibonnaci/test/fibonnaciTest.js
+++ Fibonnaci/test/fibonnaciTest.js
@@ -1,23 +1,27 @@
 const fibonnaciNumbers = require('../src/fibonnaci');
-const should = require('chai').should()
+const should = require('chai').should();
 
 describe('Fibonnaci numbers', () => {
     it('should return 1 for first fibonnaci number', () => {
-        fibonnaciNumbers(1, 1).should.deep.equal([1])
+        fibonnaciNumbers(1, 1).should.deep.equal([1]);
     })
 
     it('should return 1 for second fibonnaci number', () => {
-        fibonnaciNumbers(2, 2).should.deep.equal([1])
+        fibonnaciNumbers(2, 2).should.deep.equal([1]);
     })
 
     it('should return 8 form 6th fibonnaci number', () => {
-        fibonnaciNumbers(8, 8).should.deep.equal([21])
+        fibonnaciNumbers(8, 8).should.deep.equal([21]);
     })
 
     it('should work for large range numbers', () => {
-        let expectedResult = [1, 1, 2, 3, 5, 8, 13, 21, 34, 55]
-        fibonnaciNumbers(1, 10).should.deep.equal(expectedResult)
+        let expectedResult = [1, 1, 2, 3, 5, 8, 13, 21, 34, 55];
+        fibonnaciNumbers(1, 10).should.deep.equal(expectedResult);
     })
 
+    it('should work for very big input values', () => {
+        let expectedResult = 53316291173;
+        fibonnaciNumbers(53, 53).should.deep.equal([expectedResult]);
+    })
 
 })
