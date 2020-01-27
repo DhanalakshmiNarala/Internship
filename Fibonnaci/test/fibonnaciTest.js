cce596c20db5a73d7ecf8d5766063ce9d751a9a3
Dhanalakshmi Narala <dhanalakshmi.narala@everest.engineering>
2020-01-27 10:47:54 +0530

    Add unit test for checking second fibonnaci number

--- Fibonnaci/test/fibonnaciTest.js
+++ Fibonnaci/test/fibonnaciTest.js
@@ -2,7 +2,11 @@ const fibonnaciNumbers = require('../src/fibonnaci');
 const should = require('chai').should()
 
 describe('Fibonnaci numbers', () => {
-    it('should return 1 for first fibonnaci number', ()=> {
+    it('should return 1 for first fibonnaci number', () => {
         fibonnaciNumbers(1, 1).should.deep.equal([1])
+    })
+
+    it('should return 1 for second fibonnaci number', () => {
+        fibonnaciNumbers(2, 2).should.deep.equal([1])
     })
 })
