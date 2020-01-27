68a3fa4df285143f6523af1d2d9252227e439507
Dhanalakshmi Narala <dhanalakshmi.narala@everest.engineering>
2020-01-27 09:59:19 +0530

    Refactor the fizzBuzz code for fizz case

--- FizzBuzz/src/fizzBuzz.js
+++ FizzBuzz/src/fizzBuzz.js
@@ -1,1 +1,14 @@
-module.exports = null 
+const fizzBuzz = (startRange, endRange)=> {
+    let result = []
+    for(let num = startRange; num <= endRange; num++) {
+        result.push(getText(num))
+    }
+    return result
+}
+
+const getText = (num)=> {
+    if(num % 3 == 0)
+        return "fizz"
+}
+
+module.exports = fizzBuzz
